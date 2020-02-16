af85e8c580ca60be632727e8ae7ee6e054831c48
Dušan D. Majkić <dmajkic@lovor.net>
2020-02-17 00:00:47 +0100

    Add Time to fullcore; remove Database gem

--- gem/fullcore/fullcore.go
+++ gem/fullcore/fullcore.go
@@ -3,11 +3,12 @@ package fullcore
 import (
 	_ "github.com/oruby/oruby/gem/base64"
 	_ "github.com/oruby/oruby/gem/complex"
-	_ "github.com/oruby/oruby/gem/database"
+	//_ "github.com/oruby/oruby/gem/database"
 	_ "github.com/oruby/oruby/gem/erubi"
 	_ "github.com/oruby/oruby/gem/json"
 	_ "github.com/oruby/oruby/gem/load"
 	//	_ "github.com/oruby/oruby/gem/process"
 	_ "github.com/oruby/oruby/gem/regexp"
+	_ "github.com/oruby/oruby/gem/time"
 	_ "github.com/oruby/oruby/gem/zlib"
 )
