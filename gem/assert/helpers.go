b0ee6098689a2fc308d02907c62ac832258ff14e
Dušan D. Majkić <dmajkic@lovor.net>
2020-04-05 20:45:12 +0200

    Add slice and oruby.Value helpers

--- gem/assert/helpers.go
+++ gem/assert/helpers.go
@@ -1,6 +1,7 @@
 package assert
 
 import (
+	"github.com/oruby/oruby"
 	"reflect"
 	"runtime"
 	"strings"
@@ -15,32 +16,91 @@ func Expect(t *testing.T, to bool, eformat string, args ...interface{}) {
 	}
 }
 
-// Expect is simple testing function which raises error if condition is not met
-func Include(t *testing.T, v1 interface{}, in ...interface{}) {
-	t.Helper()
-
+func isIncluded(v1 interface{}, in ...interface{}) bool {
 	for _,v2 := range in {
-		if reflect.DeepEqual(v1, v2) {
-			return
+		if reflect.DeepEqual(toInterface(v1), toInterface(v2)) {
+			return true
 		}
 	}
-	t.Errorf("Expected '%v' to be in %v", v1, in)
+
+	if len(in) == 1 {
+		if v := reflect.ValueOf(in[0]); v.Kind() == reflect.Slice {
+			for i := 0; i < v.Len(); i++ {
+				if reflect.DeepEqual(toInterface(v1), toInterface(v.Index(i).Interface())) {
+					return true
+				}
+			}
+		}
+	}
+
+	return false
+}
+
+// Include expects item to be included in
+func Include(t *testing.T, v1 interface{}, in ...interface{}) {
+	t.Helper()
+	if !isIncluded(v1, in...) {
+		t.Errorf("Expected '%v' to be in %v", v1, in)
+	}
+}
+
+// NotInclude expects item to NOT be included in
+func NotInclude(t *testing.T, v1 interface{}, in ...interface{}) {
+	t.Helper()
+	if isIncluded(v1, in...) {
+		t.Errorf("Expected '%v' NOT to be in %v", v1, in)
+	}
+}
+
+func toInterface(v interface{}) interface{} {
+	switch t := v.(type) {
+	case oruby.RObject:
+		return t.Interface()
+	case oruby.Value:
+		switch t.Type() {
+		case oruby.MrbTTString:
+			return t.String()
+		case oruby.MrbTTFixnum:
+			return t.Int()
+		case oruby.MrbTTFalse:
+			if t.IsNil() {
+				return nil
+			}
+			return false
+		case oruby.MrbTTTrue:
+			return true
+		case oruby.MrbTTCptr:
+			return t.Uintptr()
+		}
+		return t
+	case oruby.MrbValue:
+		return toInterface(t.Value())
+	}
+	return v
 }
 
 // ExpectEql expects both arguments to be equal
 // Internaly uses reflection.DeepEqual to perform test
 func Equal(t *testing.T, v1, v2 interface{}) {
 	t.Helper()
-	Expect(t, reflect.DeepEqual(v1, v2), "Expected '%v' to equal '%v'", v1, v2)
-	//expect(t, (v1 == v2), "Expected %v to equal %v", v1, v2)
+
+	if tmp, ok := v2.(oruby.RObject); ok {
+		v2 = tmp.Interface()
+	}
+
+	Expect(t, reflect.DeepEqual(toInterface(v1), toInterface(v2)), "\nExpected '%v' \nto equal '%v'", v1, v2)
 }
 
-// EqualE
-func EqualE(t *testing.T, v1, err error, v2 interface{}) {
+// True  expects argument to be true
+func True(t *testing.T, v1 interface{}) {
 	t.Helper()
-	NilError(t, err)
-	Expect(t, reflect.DeepEqual(v1, v2), "Expected '%v' to equal '%v'", v1, v2)
-	//expect(t, (v1 == v2), "Expected %v to equal %v", v1, v2)
+	Expect(t, reflect.DeepEqual(toInterface(v1), true), "Expected '%v' to be true", v1)
+}
+
+// False  expects argument to be true
+func False(t *testing.T, v1 interface{}) {
+	t.Helper()
+	Expect(t, reflect.DeepEqual(toInterface(v1), false), "Expected '%v' to be false", v1)
 }
 
 // Nil should be used to check returned Go error.
